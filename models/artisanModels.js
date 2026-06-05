df0a7c61b6d56c7a01a130ebcb7c21cde403fe33
TristanVairet <tristan.vairet@gmail.com>
2026-06-05 11:21:11 +0200

    create models

---
+++ models/artisanModels.js
@@ -0,0 +1,62 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Artisan = sequelize.define('Artisans', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  speciality: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  grade: {
+    type: DataTypes.DECIMAL(5,2),
+    allowNull: true,
+  },
+  city: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  about: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+  },
+  email: {
+    type: DataTypes.STRING(150),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  website: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+    defaultValue: 'https://',
+    validate: {
+      isUrl: {
+        msg: 'L’URL du site web doit être valide (ex : https://monsite.com)',
+      },
+    },
+  },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  top: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  }
+}, {
+  tableName: 'artisans', // Facultatif si nom différent du modèle
+  timestamps: true,   // Crée createdAt et updatedAt automatiquement
+});
+
+module.exports = Artisan;
---
+++ models/specialityModels.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Speciality = sequelize.define('Specialities', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+  },
+}, {
+  tableName: 'specialities', // Facultatif si nom différent du modèle
+});
+
+module.exports = Speciality;
