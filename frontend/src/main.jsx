52e8a49c68baf380bcc4f3f00b687b74ce517f25
TristanVairet <tristan.vairet@gmail.com>
2026-08-19 09:31:07 +0200

    correc erreur

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/footer.jsx'
-
 
 createRoot(document.getElementById('root')).render(
    <StrictMode>
